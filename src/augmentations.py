f0aa8f3a6b251a6ac25ab54ecd5eb9df2a02b4d2
Dylan Yung <dyung6@gatech.edu>
2022-10-31 12:18:46 -0400

    added splice_conv

--- src/augmentations.py
+++ src/augmentations.py
@@ -429,6 +429,25 @@ def CS_splice(x, hue_thres=3.5, sat_thres=0, val_thres=0):
     return overlay.reshape(n, c, h, w) * 255.0
 
 
+def splice_conv(x, hue_thres=3.5, sat_thres=0, val_thres=0):
+    global data_iter
+    load_dataloader(batch_size=x.size(0), image_size=x.size(-1))
+    overlay = _get_data_batch(x.size(0)).repeat(x.size(1) // 3, 1, 1, 1)
+    n, c, h, w = x.shape
+    x_rgb = x.reshape(-1, 3, h, w) / 255.0
+    mask = create_hsv_mask(
+        x_rgb=x_rgb, hue_thres=hue_thres, sat_thres=sat_thres, val_thres=val_thres
+    )
+    mask2 = create_hsv_mask(
+        x_rgb=x_rgb, hue_thres=0, sat_thres=sat_thres, val_thres=0.6
+    )
+    conv = random_conv(x=x)
+    conv = conv.reshape(-1, 3, h, w) / 255.0
+    overlay[mask2] = conv[mask2] 
+    overlay[~mask] = conv[~mask] 
+    return overlay.reshape(n, c, h, w) * 255.0
+
+
 def emphasize(x, hue_thres=3.5, sat_thres=0, val_thres=0):
     n, c, h, w = x.shape
     x_rgb = x.reshape(-1, 3, h, w) / 255.0
@@ -653,5 +672,6 @@ aug_to_func = {
     "DrQ2_random_shift": DrQ2_random_shift,
     "mix_up": mix_up,
     "overlay_np": random_np_overlay,
-    "emphasize": emphasize
+    "emphasize": emphasize,
+    "splice_conv": splice_conv,
 }
