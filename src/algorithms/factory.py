670f054bcd4722c6b20f5d8560a11e9ef212351c
Dylan Yung <dyung6@gatech.edu>
2022-11-14 01:21:55 -0500

    removed unused models

--- src/algorithms/factory.py
+++ src/algorithms/factory.py
@@ -1,5 +1,4 @@
 from algorithms.alix import ALIX
-from algorithms.curriculum_learning.secant import SECANT
 from algorithms.non_naive_rad2 import NonNaiveRAD2
 from algorithms.rad2 import RAD2
 from algorithms.rad_alix import RAD_ALIX
@@ -12,10 +11,8 @@ from algorithms.soda import SODA
 from algorithms.drq import DrQ
 from algorithms.svea import SVEA
 from algorithms.transfer import Transfer
-from algorithms.curriculum_learning.curriculum import Curriculum
 from algorithms.curriculum_learning.curriculum_double import Curriculum_Double
 from algorithms.non_naive_rad import NonNaiveRAD
-from algorithms.sac_policy_eval import SAC_policy_eval
 from algorithms.SARSA_policy_eval import SARSA_policy_eval
 from algorithms.curriculum_learning.curriculum_FTL import Curriculum_FTL
 from algorithms.curriculum_learning.curriculum_single import CurriculumSingle
@@ -30,15 +27,12 @@ algorithm = {
     "drq": DrQ,
     "svea": SVEA,
     "transfer": Transfer,
-    "curriculum": Curriculum,
     "non_naive_rad": NonNaiveRAD,
     "augcl": Curriculum_Double,
-    "sac_policy_eval": SAC_policy_eval,
     "sarsa_policy_eval": SARSA_policy_eval,
     "curriculum_FTL": Curriculum_FTL,
     "curriculum_single": CurriculumSingle,
     "2x_fresh": CurriculumFresh,
-    "secant": SECANT,
     "rad2": RAD2,
     "non_naive_rad2": NonNaiveRAD2,
     "alix": ALIX,
