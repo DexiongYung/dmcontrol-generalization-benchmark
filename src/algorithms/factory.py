e10e25ca8ff16eb9cb2755e48c3542a1b915e423
Dylan Yung <dyung6@gatech.edu>
2022-11-13 20:16:37 -0500

    removed unused models

--- src/algorithms/factory.py
+++ src/algorithms/factory.py
@@ -1,7 +1,5 @@
 from algorithms.alix import ALIX
-from algorithms.curriculum_learning.augcl4 import AugCL4
 from algorithms.curriculum_learning.secant import SECANT
-from algorithms.non_naive_drq import Non_Naive_DrQ
 from algorithms.non_naive_rad2 import NonNaiveRAD2
 from algorithms.rad2 import RAD2
 from algorithms.rad_alix import RAD_ALIX
@@ -13,7 +11,6 @@ from algorithms.pad import PAD
 from algorithms.soda import SODA
 from algorithms.drq import DrQ
 from algorithms.svea import SVEA
-from algorithms.svea2 import SVEA2
 from algorithms.transfer import Transfer
 from algorithms.curriculum_learning.curriculum import Curriculum
 from algorithms.curriculum_learning.curriculum_double import Curriculum_Double
@@ -21,13 +18,8 @@ from algorithms.non_naive_rad import NonNaiveRAD
 from algorithms.sac_policy_eval import SAC_policy_eval
 from algorithms.SARSA_policy_eval import SARSA_policy_eval
 from algorithms.curriculum_learning.curriculum_FTL import Curriculum_FTL
-from algorithms.curriculum_learning.curriculum_resample import Curriculum_Resample
-from algorithms.curriculum_learning.curriculum_2x_nu_actor import Curriculum_2x_Nu_Actor
-from algorithms.curriculum_learning.curriculum_2x_opt import Curriculum_2x_Opt
-from algorithms.curriculum_learning.curriculum_bb import Curriculum_BB
 from algorithms.curriculum_learning.curriculum_single import CurriculumSingle
 from algorithms.curriculum_learning.curriculum_fresh import CurriculumFresh
-from algorithms.curriculum_learning.augcl2 import AugCL2
 
 algorithm = {
     "sac": SAC,
@@ -44,22 +36,14 @@ algorithm = {
     "sac_policy_eval": SAC_policy_eval,
     "sarsa_policy_eval": SARSA_policy_eval,
     "curriculum_FTL": Curriculum_FTL,
-    "curriculum_resample": Curriculum_Resample,
-    "2x_curriculum_nu_actor": Curriculum_2x_Nu_Actor,
-    "2x_curriculum_opt": Curriculum_2x_Opt,
-    "2x_curriculum_bb": Curriculum_BB,
     "curriculum_single": CurriculumSingle,
     "2x_fresh": CurriculumFresh,
     "secant": SECANT,
-    "augcl2": AugCL2,
-    "svea2": SVEA2,
     "rad2": RAD2,
     "non_naive_rad2": NonNaiveRAD2,
     "alix": ALIX,
     "rad_alix": RAD_ALIX,
     "rad_shift": RAD_shift,
-    "non_naive_drq": Non_Naive_DrQ,
-    "augcl4": AugCL4,
 }
 
 
--- src/algorithms/non_naive_rad2.py
+++ src/algorithms/non_naive_rad2.py
@@ -6,6 +6,24 @@ class NonNaiveRAD2(RAD):
     def __init__(self, obs_shape, action_shape, args):
         super().__init__(obs_shape, action_shape, args)
 
+    def update_critic(self, obs, action, reward, next_obs, not_done, L=None, step=None):
+        with torch.no_grad():
+            _, policy_action, log_pi, _ = self.actor(self.apply_aug(next_obs))
+            target_Q1, target_Q2 = self.critic_target(next_obs, policy_action)
+            target_V = torch.min(target_Q1, target_Q2) - self.alpha.detach() * log_pi
+            target_Q = reward + (not_done * self.discount * target_V)
+
+        current_Q1, current_Q2 = self.critic(obs, action)
+        critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(
+            current_Q2, target_Q
+        )
+        if L is not None:
+            L.log("train_critic/loss", critic_loss, step)
+
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
     def update(self, replay_buffer, L, step):
         obs, action, reward, next_obs, not_done = replay_buffer.sample()
         aug_obs = self.apply_aug(obs)
@@ -17,9 +35,3 @@ class NonNaiveRAD2(RAD):
 
         if step % self.critic_target_update_freq == 0:
             self.soft_update_critic_target()
-
-    def sample_action(self, obs):
-        _obs = self._obs_to_input(obs)
-        with torch.no_grad():
-            mu, pi, _, _ = self.actor(self.apply_aug(_obs), compute_log_pi=False)
-        return pi.cpu().data.numpy().flatten()
